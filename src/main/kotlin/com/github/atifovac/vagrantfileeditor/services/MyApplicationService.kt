3000abf456fafe358367441ca55d36a312e8b3f5
GitHub Action <action@github.com>
2021-03-03 15:13:08 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/atifovac/vagrantfileeditor/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.atifovac.vagrantfileeditor.services
+
+import com.github.atifovac.vagrantfileeditor.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
